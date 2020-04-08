86507b87b24b1965f4dbdb1256bbb73c15e9d262
Christian Montero Martínez <christianmontero08@gmail.com>
2020-04-07 19:42:13 -0700

    Import custom fonts

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import * as Font from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home';
+import { AppLoading } from 'expo'
+
+const getFonts = () => Font.loadAsync({
+  'ptsans-regular': require('./assets/fonts/PTSans-Regular.ttf'),
+  'ptsans-bold': require('./assets/fonts/PTSans-Bold.ttf')
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded){
+    return (
+      <Home/>
+    );
+  }else{
+    return(
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
+  
+}
