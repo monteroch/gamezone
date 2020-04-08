8e04f3185a890c19c8082f1ce6a0a338fd278bb7
Christian Montero Martínez <christianmontero08@gmail.com>
2020-04-07 22:04:01 -0700

    Implement functionality for navigate around screens and pass data

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export function Home(){
+export function Home({navigation}){
+
+    const [reviews, setReviews] = useState([
+        {title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1'},
+        {title: 'Gotta Catch The All', rating: 4, body: 'lorem ipsum', key: '2'},
+        {title: 'Final Fantasy', rating: 3, body: 'lorem ipsum', key: '3'},
+    ]);
+
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails');
+    }
+
     return(
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
+            <FlatList
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
